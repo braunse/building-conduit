5231a1b245a6b89a5d8b10d6d5e655ad16f87dd1
Sebastien Braun <sebastien@sebbraun.de>
2020-11-25 08:43:40 +0100

    Fix wrong module name

--- lib/conduit/supervisor.ex
+++ lib/conduit/supervisor.ex
@@ -1,4 +1,4 @@
-defmodule Conduit.Application.Supervisor do
+defmodule Conduit.Supervisor do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
